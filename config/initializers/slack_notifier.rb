0c0cc79f09370854c3b825fb2f7a30e93c299963
Harry Yuan <harryuan.65@gmail.com>
2020-01-19 21:53:18 +0800

    fix slack notifier 2/n

--- config/initializers/slack_notifier.rb
+++ config/initializers/slack_notifier.rb
@@ -1,5 +1,5 @@
 module SlackNotifier
-    CLIENT = Slack::Notifier.new "https://hooks.slack.com/services/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" do
+    CLIENT = Slack::Notifier.new ENV["SLACK_WEBHOOK"] do
         defaults channel: "#smallbai"
     end
 end

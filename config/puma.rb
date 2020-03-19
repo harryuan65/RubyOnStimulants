b6449091a8536ba177c109a6b555f8057a1cdda2
Harry Yuan <harryuan.65@gmail.com>
2020-03-19 15:50:21 +0800

    remove console block

--- config/puma.rb
+++ config/puma.rb
@@ -34,12 +34,6 @@ environment ENV.fetch("RAILS_ENV") { "development" }
 # Allow puma to be restarted by `rails restart` command.
 plugin :tmp_restart
 
-console do
-  # Worker specific setup for Rails 4.1+
-  # See: https://devcenter.heroku.com/articles/deploying-rails-applications-with-the-puma-web-server#on-worker-boot
-  ActiveRecord::Base.establish_connection
-end
-
 on_worker_boot do
   t = Thread.new do
     puts "[Wakeup]Started counter"

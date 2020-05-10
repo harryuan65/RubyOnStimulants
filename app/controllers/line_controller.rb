a4121acfa3f042c9f95346d7471c423cd93fa505
Harry Yuan <harryuan.65@gmail.com>
2020-05-10 15:12:39 +0800

    fix client

--- app/controllers/line_controller.rb
+++ app/controllers/line_controller.rb
@@ -5,11 +5,11 @@ class LineController < ApplicationController
       puts body
 
       signature = request.env['HTTP_X_LINE_SIGNATURE']
-      unless client.validate_signature(body, signature)
-        error 400 do 'Bad Request' end
+      unless $client.validate_signature(body, signature)
+       render plain: 'Bad request', status: :bad_request
       end
 
-      events = client.parse_events_from(body)
+      events = $client.parse_events_from(body)
       events.each do |event|
         case event
         when Line::Bot::Event::Message
@@ -19,9 +19,9 @@ class LineController < ApplicationController
               type: 'text',
               text: event.message['text']
             }
-            client.reply_message(event['replyToken'], message)
+            $client.reply_message(event['replyToken'], message)
           when Line::Bot::Event::MessageType::Image, Line::Bot::Event::MessageType::Video
-            response = client.get_message_content(event.message['id'])
+            response = $client.get_message_content(event.message['id'])
             tf = Tempfile.open("content")
             tf.write(response.body)
           end

94da07d51ac38920d9eb17443d6ebe8474ddfd7b
Harry Yuan <harryuan.65@gmail.com>
2020-05-25 14:58:07 +0800

    order words, center zh definition

--- app/models/definition.rb
+++ app/models/definition.rb
@@ -13,5 +13,5 @@ class Definition < ApplicationRecord
   belongs_to :word
   has_many :examples
   has_one :first_example, ->{ order(id: :asc).limit(1) }, class_name: 'Example'
-  validates :part_of_speech, acceptance: { accept: ['noun', 'verb', 'adjective', 'adverb', 'preposition', 'pronoun', 'interjection', 'conjunction','exclamation'] }
+  validates :part_of_speech, acceptance: { accept: ['noun', 'verb', 'adjective', 'adverb', 'preposition', 'pronoun', 'interjection', 'conjunction','exclamation', 'suffix', 'prefix'] }
 end

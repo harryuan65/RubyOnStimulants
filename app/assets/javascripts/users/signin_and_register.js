437d3c9f818aaaaaeaaab94f86c53c9c21f0a709
Harry Yuan <harryuan.65@gmail.com>
2020-09-11 22:28:28 +0800

    feat: refactor signup page alert and js password validation

--- app/assets/javascripts/users/signin_and_register.js
+++ app/assets/javascripts/users/signin_and_register.js
@@ -10,5 +10,17 @@ document.addEventListener('DOMContentLoaded', ()=>{
             label.classList.toggle('focus');
             e.target.classList.toggle('focus');
         })
-    })
+    });
+    // Want to use I18n in javascript -> put in view as javascript_tag
+    // let signupForm = document.getElementById('form-signup');
+    // if (signupForm){
+    //   signupForm.addEventListener('submit',(event)=>{
+    //     event.preventDefault();
+    //     let pwd = signupForm.querySelector('#user_password');
+    //     let pwdConfirmation = signupForm.querySelector('#user_password_confirmation');
+    //     if (pwd!==pwdConfirmation){
+    //       alert("<%= I18n.t('devise.registration.password_inconsistent') %>");
+    //     }
+    //   });
+    // }
 })

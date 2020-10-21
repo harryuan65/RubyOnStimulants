6385f94bfbd127e063305b087719af96fae66848
Harry Yuan <harryuan.65@gmail.com>
2020-10-21 17:22:36 +0800

    fix: no locale error#2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,7 +13,7 @@ class ApplicationController < ActionController::Base
   def switch_locale(&action)
     logger.debug "* Accept-Language: #{request.env['HTTP_ACCEPT_LANGUAGE']}"
     locale = extract_locale_from_accept_language_header
-    if I18n.available_locales.excludes?(locale)
+    if I18n.available_locales.exclude?(locale)
       locale = :en
       logger.debug "* Locale not supported. Set to '#{locale}'"
     else

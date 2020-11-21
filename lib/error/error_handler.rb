df94556ab98a29c6608ed4d3fff308819098438a
Harry Yuan <harryuan.65@gmail.com>
2020-11-21 21:27:11 +0800

    chore: rename error handlers

--- lib/error/error_handler.rb
+++ lib/error/error_handler.rb
@@ -2,10 +2,10 @@ module Error
   module ErrorHandler
     def self.included(origin_class)
       origin_class.class_eval do
-        rescue_from StandardError, with: :server_error_json
-        rescue_from ActiveRecord::RecordInvalid, with: :record_invalid_json
-        rescue_from ActionController::RoutingError, with: :route_not_found_json
-        rescue_from ActiveRecord::RecordNotFound, with: :record_not_found_json
+        rescue_from StandardError, with: :server_error_handler
+        rescue_from ActiveRecord::RecordInvalid, with: :record_invalid_handler
+        rescue_from ActionController::RoutingError, with: :route_not_found_handler
+        rescue_from ActiveRecord::RecordNotFound, with: :record_not_found_handler
         rescue_from CustomError do |error| # handle starts
           handle status: error.status,
                  i18n_message: error.i18n_message,
@@ -15,22 +15,22 @@ module Error
     end
 
     private
-    def record_not_found_json(exception)
+    def record_not_found_handler(exception)
       handle i18n_message: I18n.t("controller.general.record_not_found"), error_message: exception.to_s, status: :not_found
     end
 
-    def record_invalid_json(exception)
+    def record_invalid_handler(exception)
       handle i18n_message: I18n.t("controller.general.record_invalid"), error_message: exception.to_s, status: :bad_request
     end
 
-    def server_error_json(exception)
+    def server_error_handler(exception)
       Rollbar.error(exception)
-      logger.fatal exception.to_s
-      logger.fatal exception.backtrace[0]
-      handle i18n_message: I18n.t("controller.general.internal_server_error"), error_message: exception.to_s, status: :internal_server_error
+      error_details = exception.to_s + "\n" + exception.backtrace[0]
+      logger.fatal error_details
+      handle i18n_message: I18n.t("controller.general.internal_server_error"), error_message: error_details, status: :internal_server_error
     end
 
-    def route_not_found_json(exception)
+    def route_not_found_handler(exception)
       handle i18n_message: I18n.t("controller.general.route_not_found"), error_message: exception.to_s, status: :not_found
     end
 

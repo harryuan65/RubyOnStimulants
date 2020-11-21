d6cd3052e2f8bc866d28d255950be67945cf4594
Harry Yuan <harryuan.65@gmail.com>
2020-11-21 18:57:29 +0800

    feat: add intro to users, update articles#show author

---
+++ db/migrate/20201121085402_add_intro_to_users.rb
@@ -0,0 +1,24 @@
+class AddIntroToUsers < ActiveRecord::Migration[6.0]
+  def change
+    reversible do |migration|
+      migration.up do
+        execute(
+        <<~SQL
+          ALTER TABLE users ADD COLUMN intro VARCHAR(128);
+          UPDATE users
+          SET intro='rails developer'
+          WHERE email='#{ENV['ADMIN']}'
+        SQL
+      )
+      end
+
+      migration.down do
+        execute(
+          <<~SQL
+            ALTER TABLE users DROP COLUMN intro;
+          SQL
+        )
+      end
+    end
+  end
+end

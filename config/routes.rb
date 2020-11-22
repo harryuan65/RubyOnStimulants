cefc138ece211acdf85f9da2259c36a09c1c76f2
Harry Yuan <harryuan.65@gmail.com>
2020-11-22 16:12:14 +0800

    feat: add authentication to sidekiq web ui

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   require 'sidekiq/web'
-  mount Sidekiq::Web => '/sidekiq'
+  require 'sidekiq-scheduler/web'
+  authenticate :user, lambda { |u| u.admin? } do
+    mount Sidekiq::Web => '/sidekiq'
+  end
 
   devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks", sessions:"users/sessions", registrations: "users/registrations" }
 

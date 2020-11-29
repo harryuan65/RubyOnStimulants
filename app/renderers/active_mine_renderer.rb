5d01624b1023b1be135fb12cec9960217ee7c6e5
Harry Yuan <harryuan.65@gmail.com>
2020-11-29 17:13:19 +0800

    fix: rubocop lint

--- app/renderers/active_mine_renderer.rb
+++ app/renderers/active_mine_renderer.rb
@@ -1,8 +1,4 @@
 class ActiveMineRenderer < Redcarpet::Render::HTML
-  def initialize(**options)
-    super
-  end
-
   def link(link, title, content)
     %(<a href="#{link}" target="_blank">#{content}</a>)
   end

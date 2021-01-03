8d062be1ae820f9b286ef9aa3e5a28916dcdca72
Harry Yuan <harryuan.65@gmail.com>
2021-01-04 00:44:51 +0800

    fix: sprockets error

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,6 +1,6 @@
 //= link_tree ../images
 //= link_tree ../fonts
 //= link_tree ../csv
-//= link_tree ../javascripts .js
-//= link_tree ../stylesheets .css
+//= link_directory ../javascripts .js
+//= link_directory ../stylesheets .css
 
